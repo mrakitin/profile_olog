a692f9fa16d408c5bc5e95cf81fb8d404343d5e2
Maksim Rakitin <mrakitin@bnl.gov>
2022-03-18 12:26:55 -0400

    TST: add length checks; comment out the second test

--- startup/tests/test_names.py
+++ startup/tests/test_names.py
@@ -22,6 +22,8 @@ def test_body_attachment(body, newline, logid=1180):
     attachment = io.StringIO(body)
     attachment.name = fname
 
+    assert attachment.getvalue() == body
+
     olog_client = SimpleOlogClient()
 
     olog_client.update(
@@ -38,32 +40,33 @@ def test_body_attachment(body, newline, logid=1180):
 
     print(f"  text = {res.text}")
     assert res.text == body
+    assert len(res.text) == len(body)
 
 
-@pytest.mark.parametrize("body", body)
-@pytest.mark.parametrize("newline", [True, False])
-def test_body_atch(body, newline, logid=1180):
-    if newline:
-        body = f"{body}\n"
-    name = str(uuid.uuid4())
-    fname = f"{name}.txt"
-
-    atch = io.StringIO(body)
-    atch.name = fname
-
-    olog_client = SimpleOlogClient()
-
-    olog_client.update(
-        log_id=logid,
-        text="text",
-        logbooks="Data Acquisition",
-        attachments=[atch],
-    )
-
-    res_url = f"https://epics-services-tst:33181/Olog/resources/attachments/{logid}/{fname}"
-    print(f"\n  res_url = {res_url}")
-
-    res = requests.get(res_url, verify=False)
-
-    print(f"  text = {res.text}")
-    assert res.text == body
+# @pytest.mark.parametrize("body", body)
+# @pytest.mark.parametrize("newline", [True, False])
+# def test_body_atch(body, newline, logid=1180):
+#     if newline:
+#         body = f"{body}\n"
+#     name = str(uuid.uuid4())
+#     fname = f"{name}.txt"
+#
+#     atch = io.StringIO(body)
+#     atch.name = fname
+#
+#     olog_client = SimpleOlogClient()
+#
+#     olog_client.update(
+#         log_id=logid,
+#         text="text",
+#         logbooks="Data Acquisition",
+#         attachments=[atch],
+#     )
+#
+#     res_url = f"https://epics-services-tst:33181/Olog/resources/attachments/{logid}/{fname}"
+#     print(f"\n  res_url = {res_url}")
+#
+#     res = requests.get(res_url, verify=False)
+#
+#     print(f"  text = {res.text}")
+#     assert res.text == body
