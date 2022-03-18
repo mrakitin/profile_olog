4fdeebb7d9873e29a2a8f4351d33d71b3c9f1733
Maksim Rakitin <mrakitin@bnl.gov>
2022-03-18 10:45:16 -0400

    Add more tests

--- startup/tests/test_names.py
+++ startup/tests/test_names.py
@@ -5,11 +5,13 @@ import pytest
 import requests
 from pyOlog import SimpleOlogClient
 
+body = ["t", "te", "tes", "test", "test1", "test-2"]
 
-def test_attachment_names(logid=1180):
+
+@pytest.mark.parametrize("body", body)
+def test_body_attachment(body, logid=1180):
     name = str(uuid.uuid4())
     fname = f"{name}.txt"
-    body = "test"
 
     attachment = io.StringIO(body)
     attachment.name = fname
@@ -22,10 +24,37 @@ def test_attachment_names(logid=1180):
         logbooks="Data Acquisition",
         attachments=[attachment],
     )
-    res = requests.get(
-        f"https://epics-services-tst:33181/Olog/resources/attachments/{logid}/{fname}",
-        verify=False,
+
+    res_url = f"https://epics-services-tst:33181/Olog/resources/attachments/{logid}/{fname}"
+    print(f"\n  res_url = {res_url}")
+
+    res = requests.get(res_url, verify=False)
+
+    print(f"  text = {res.text}")
+
+
+@pytest.mark.parametrize("body", body)
+def test_body_atch(body, logid=1180):
+    name = str(uuid.uuid4())
+    fname = f"{name}.txt"
+
+    atch = io.StringIO(body)
+    atch.name = fname
+
+    olog_client = SimpleOlogClient()
+
+    olog_client.update(
+        log_id=logid,
+        text="text",
+        logbooks="Data Acquisition",
+        attachments=[atch],
     )
 
-    print(res.text)
+    res_url = f"https://epics-services-tst:33181/Olog/resources/attachments/{logid}/{fname}"
+    print(f"\n  res_url = {res_url}")
+
+    res = requests.get(res_url, verify=False)
+
+    print(f"  text = {res.text}")
+    assert res.text == body
     assert res.text == body
