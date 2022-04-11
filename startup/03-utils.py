55603a232c620fce4f41fe07548ed4ad5edff0cf
Maksim Rakitin <mrakitin@bnl.gov>
2022-04-11 12:40:58 -0400

    Update plotting code to accept various beamlines/ports

--- startup/03-utils.py
+++ startup/03-utils.py
@@ -4,22 +4,23 @@ import matplotlib.pyplot as plt
 plt.ion()
 
 
-def get_latest_log():
+def get_latest_log(bl="tst", port="33181"):
     res = requests.get(
-        f"https://epics-services-tst:33181/Olog/resources/logs?limit=1",
+        f"https://epics-services-{bl}:{port}/Olog/resources/logs?limit=1",
         verify=False,
     )
     data = res.json()
-    return data
+    return (data, res.url)
 
 
-def plot_sizes():
-    data = get_latest_log()
-    data = data[0]["attachments"]
+def plot_sizes(*args, **kwargs):
+    (data_raw, url) = get_latest_log(*args, **kwargs)
+    data = data_raw[0]["attachments"]
     sizes = [d["fileSize"] for d in data]
     print(f"Number of sizes: {len(sizes)}")
 
     plt.plot(sizes)
+    plt.title(f"URL: {url}\nOlog ID: {data_raw[0]['id']}")
     plt.xlabel(f"File counter (0-{len(data)-1})")
     plt.ylabel("File size [bytes]")
     plt.grid(visible=True)
